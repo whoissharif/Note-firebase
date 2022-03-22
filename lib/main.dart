db7b89d63d11c60c6ed6ce37b1202c162241012b
whoissharif <sharif.csebubt@gmail.com>
2022-03-22 16:05:55 +0600

    data fetched from firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -10,11 +14,55 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({Key? key}) : super(key: key);
+
+  final Stream<QuerySnapshot> notes =
+      FirebaseFirestore.instance.collection('notes').snapshots();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fire Note'),
+      ),
+      body: Center(
+        child: StreamBuilder(
+          stream: notes,
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return const Text('Something went wrong');
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Text('Loading');
+            }
+
+            final data = snapshot.requireData;
+
+            return ListView.builder(
+                itemCount: data.size,
+                itemBuilder: ((context, index) {
+                  return ListTile(
+                    title: Text(
+                      data.docs[index]['title'],
+                    ),
+                    subtitle: Text(data.docs[index]['description']),
+                  );
+                }));
+          },
+        ),
+      ),
     );
   }
 }
